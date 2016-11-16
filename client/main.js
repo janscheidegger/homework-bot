97ce400147daf5a456f1944163e8818a42573b7a
Dominique Schuwey <domi@domidesign.ch>
2016-11-16 22:51:09 +0100

    fix merge conflicts

--- client/main.js
+++ client/main.js
@@ -1,18 +1,26 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Classes } from '../imports/api/classes.js';
+import { HTTP } from 'meteor/http';
 
 import './main.html';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
+  setInterval();
 });
 
 Template.hello.helpers({
   counter() {
     return Template.instance().counter.get();
   },
+
+  setInterval(function () {
+    HTTP.get("https://api.telegram.org/bot286533420:AAGOTzFbQ4amPaSwe6nJlYu08EuvnrKd-UI/getUpdates", [], function(result) {
+      console.log(result);
+    });
+  }, 1000)
 });
 
 Template.hello.events({
@@ -20,6 +28,15 @@ Template.hello.events({
     // increment the counter when button is clicked
     instance.counter.set(instance.counter.get() + 1);
   },
+
+  'click .js-sendMessage'(event) {
+    HTTP.call("POST", "https://api.telegram.org/bot286533420:AAGOTzFbQ4amPaSwe6nJlYu08EuvnrKd-UI/sendMessage",
+          {data: {text: "Hello World", chat_id: -168485478}},
+          function (error, result) {
+            console.log(result);
+          });
+  },
+
 });
 
 Template.classes.helpers({
@@ -62,15 +79,15 @@ Template.currentClass.events({
 Template.classes.events({
   'submit .new-class'(event) {
     event.preventDefault();
- 
+
     const target = event.target;
     const className = target.classname.value;
- 
+
     Classes.insert({
       className,
       createdAt: new Date(),
     });
- 
+
      target.classname.value = '';
   },
 });
--- server/main.js
+++ server/main.js
@@ -5,4 +5,9 @@ import '../imports/api/classes.js';
 Meteor.startup(() => {
   
   // code to run on server at startup
+
+
+  // https://api.telegram.org/bot286533420:AAGOTzFbQ4amPaSwe6nJlYu08EuvnrKd-UI/getMe
+
+  // channelId: g168485478
 });
