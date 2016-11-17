100f116ae7581822f69db65126292f8cb1e69b56
Jan Scheidegger <scheideggerj@gmail.com>
2016-11-17 13:03:41 +0100

    poll data from telegram chat

--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { HTTP } from 'meteor/http';
 import '../imports/api/classes.js';
 
 
@@ -10,4 +11,24 @@ Meteor.startup(() => {
   // https://api.telegram.org/bot286533420:AAGOTzFbQ4amPaSwe6nJlYu08EuvnrKd-UI/getMe
 
   // channelId: g168485478
+  Meteor.setInterval(function() {
+    poll();
+  }, 10000
+);
+  
 });
+
+function poll() {
+
+let callOptions = {
+  timeout: 10,
+  offset:2
+}
+
+  HTTP.post("https://api.telegram.org/bot286533420:AAGOTzFbQ4amPaSwe6nJlYu08EuvnrKd-UI/getUpdates", {data: callOptions }, function(error, result) {
+      console.log(result);
+      console.log(result.data.result);
+      console.log(error);
+    });
+}
+
